36820775ad7479dec7e11bb7130efcc2cd889171
Fangyuan Zhou <uimicro@outlook.com>
2025-08-22 13:33:40 +0800

    M PIL/utils.py

--- PIL/utils.py
+++ PIL/utils.py
@@ -1,6 +1,7 @@
 import numpy as np
 from PIL import Image
 
+# example: timg(batch['obs']['left_wrist_img'][0][0]).save('hello.jpg')
 # torch.Tensor
 def timg(img):
     return Image.fromarray((img.cpu().numpy().transpose(1, 2, 0) * 255).astype(np.uint8))
